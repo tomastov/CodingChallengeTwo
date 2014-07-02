55d7da2bdada9eec2bb195c394f2fc016ee43f77
Tomas Vaskevicius <Tomas.Vaskevicius@softwire.com>
2014-07-02 17:51:43 +0100

    message

--- CoddingChallengeTwo/Program.cs
+++ CoddingChallengeTwo/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CoddingChallengeTwo
+﻿namespace CoddingChallengeTwo
 {
-    class Program
+    using System;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.Write("Enter the file name: ");
+            var fileName = Console.ReadLine();
+            Console.Write("Enter a frequency: ");
+            var frequency = Convert.ToInt32(Console.ReadLine());
+
+            var numberOfTLS = 10;
+
+            Console.Write("There are {1} TLSs that appear in this document exactly {2} times.", numberOfTLS, frequency);
         }
     }
 }
